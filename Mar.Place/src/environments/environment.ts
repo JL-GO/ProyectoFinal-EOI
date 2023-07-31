37b785db0bbdd7a6e6af8b63fea5351bbc91a605
JL-GO <82835835+JL-GO@users.noreply.github.com>
2023-07-31 19:11:05 +0200

    Add files via upload

---
+++ Mar.Place/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    URL_AUTH: 'https://appstic.eu/marketplace/login',
+    URL_USUARIO: 'https://appstic.eu/marketplace/usuario',
+    URL_ARTICULO: 'https://appstic.eu/marketplace/articulo',
+    URL_PEDIDO: 'https://appstic.eu/marketplace/pedido'
+};
